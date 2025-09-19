0e1fe927990554b2ce1406322f52c86fbf5b4b1d
Shivamash12 <152767952+Shivamash12@users.noreply.github.com>
2025-09-19 06:16:05 +0000

    DataType-Summary

--- 01_basic/05_datatype-summary.js
+++ 01_basic/05_datatype-summary.js
@@ -33,3 +33,25 @@ console.log(typeof bigNumber);
 console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "shashwatmaurya12.com"
+let anotherName = myYoutubeName
+anotherName = "Chaiaurcode"
+console.log(myYoutubeName)
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@sbl"
+}
+
+let userTwo  = userOne 
+userTwo.email = "shashwatmaurya12@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email)
