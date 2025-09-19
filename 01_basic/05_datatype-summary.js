e8ce701edb5b3bb52f240f7c37bd7952067f0dbb
Shivamash12 <152767952+Shivamash12@users.noreply.github.com>
2025-09-19 05:58:44 +0000

    DataType-Summary

---
+++ 01_basic/05_datatype-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 categries : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol
+
+// console.log( id === anotherId);
+
+// const bigNumber = 3423423423432234234424n
+
+// Reference (Non Primitive)
+
+// Arrays, Objects, Functions
+
+// const heros = ["Shaktiman", "Maagraj", "doga"]
+// let myObj = {
+//     name : "Shivam",
+//     age : 22,
+// }
+
+const myFunction = function(){
+    console.log("Hello Javacript");
+}
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
