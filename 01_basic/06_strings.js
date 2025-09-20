0a46cbda38dc873f3c65f230a390ac1527702194
Shivamash12 <152767952+Shivamash12@users.noreply.github.com>
2025-09-20 01:51:28 +0000

    String

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,44 @@
+const name ="Shashwat"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+const gameName = new String(`Sh-ash-w-at`)
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newString1 = "   Shashwat   "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://www.google. com"
+
+console.log(url.replace('. ', '-'));
+
+console.log(url.includes('Shivam'));
+
+console.log(gameName.split('-'));
+
+   
+
+
+
+
+
+
+
+
