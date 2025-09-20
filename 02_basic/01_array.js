737c2cbbb759da441732132f811114605eaa9f5f
Shivamash12 <152767952+Shivamash12@users.noreply.github.com>
2025-09-20 21:34:39 +0000

    Array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,47 @@
+// array
+
+const myArr = [0,12,3,4,5,6]
+const myHeros = ["Shasktiman", "Naagraj"]
+
+const myArr1 = [23,12,3,4,5,6]
+console.log(myArr[0]);
+console.log(myArr1[0]);
+
+
+//  Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift(9)
+
+console.log(myArr);
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(3));
+
+console.log(myArr);
+
+const newArr = myArr.join()
+
+console.log((myArr));
+console.log(newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.slice(1,3)
+console.log("C , myay");
+console.log(myn2);
+
+
+
